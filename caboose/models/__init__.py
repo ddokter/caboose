4ab977d61ae612ff7ff90d024bec72d5c2c67106
Duco Dokter <d@etcanemtuum.org>
2024-07-31 19:51:49 +0200

    Not use tasks and services for now

--- caboose/models/__init__.py
+++ caboose/models/__init__.py
@@ -4,7 +4,7 @@ from .recipe import Recipe  # noqa
 from .event import Event  # noqa
 from .conversion import Conversion  # noqa
 from .allergen import Allergen  # noqa
-from .service import Service  # noqa
-from .task import Task  # noqa
+#  from .service import Service  # noqa
+#  from .task import Task  # noqa
 
 from .signal_processors import *  # noqa
